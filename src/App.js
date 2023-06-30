6a6c9eddf55e34b6ca00864fa4b784223dab4829
JakubKub13 <jakubkubala3@gmail.com>
2023-06-30 23:24:55 +0200

    Finishing project

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import allMovies from './data';
+import categories from './categories';
+
+const App = () => {
+  const [typeOfMovie, setTypeOfMovie] = useState("komedie")
+
+  const finalMovies = allMovies.filter( (oneMovie) => {
+    return oneMovie["category"] === typeOfMovie
+  })
+
+  console.log(finalMovies)
+
+  return (
+    <div>
+      <div className='all-buttons'>
+        {
+          categories.map( (oneCategory, index) => {
+            return <button className="one-button" key={index} onClick={ () => setTypeOfMovie(oneCategory)
+            }>{oneCategory}</button>
+          })
+        }
+      </div>
+      <div className='all-movies'>
+        {
+          finalMovies.map( (oneMovie) => {
+            const {id, image, title, age, tags, description} = oneMovie
+
+            return <div key={id} className= "one-movie">
+              <img src={image} alt="" />
+              <h2>{title}</h2>
+              <p>{age}</p>
+              <p>{tags}</p>
+              <p>{description}</p>
+              </div>
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App;
